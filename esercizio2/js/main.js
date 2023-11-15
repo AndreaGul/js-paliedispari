b810c043f398f66c2c31ed5a86c91d943e5ba1aa
AndreaGul <andreagulino99@gmail.com>
2023-11-15 02:08:10 +0100

    feat: funzione random e somma num utente e e pc

--- esercizio2/js/main.js
+++ esercizio2/js/main.js
@@ -9,6 +9,8 @@
 // Dichiariamo chi ha vinto.
 
 // definisco una variabile con un prompt che chiede all'utente di scegliere tra pari o dispari
+
+/*
 let PDUtente;
 
 //Effettuo un controllo per vedere se l'utente inserisce la parola pari o dispari, utilizzando come condizione nel while che la parola non deve essere diversa dalla parola 'pari' o da 'dispari' quando verrà inserita una delle due parole pari o dispari il ciclo si interromperà.
@@ -19,7 +21,7 @@ while (PDUtente !== 'pari' && PDUtente !== 'dispari') {
 console.log("La scelta tra pari e dispari dell'utente è =", PDUtente);
 
 // definisco un ulteriore varibile con propt che chiedere all'utente di inserire un numero da 1 a 5
-
+*/
 let numUtente = 0;
 
 //E verifico se il numero inserito e compreso tra 1 e 5 con un while che ha come condizione che il numero dell'utente deve non essere minore di 1 o maggiore di 5.
@@ -27,13 +29,23 @@ while (numUtente < 1 || numUtente > 5) {
   numUtente = +prompt('inserisci un numero da 1 a 5');
 }
 console.log("Numero inserito dall'utene =", numUtente);
+
 // (nella funzione numero random)
-// dichiaro una variabile e la definisco con un numero a caso da 1 a 5 compresi 1 e 5 grazie al metodo math.random
-// e ritorno il valore della variabile
+
+function numRandom() {
+  // dichiaro una variabile e la definisco con un numero a caso da 1 a 5 compresi 1 e 5 grazie al metodo math.random
+  const numRandom = +Math.floor(Math.random() * 5) + 1;
+  // e ritorno il valore della variabile
+  return numRandom;
+}
 
 // il contenuto ritornato della funzione lo salvo in una variabile
 
+const numComputer = numRandom();
+console.log('Numero generato per il computer =', numComputer);
+
 // dichiaro una variabile e la definisco come la somma del numero inserire dall'utente e il numero che viene ritornato dalla funzione
+const sumNum = numUtente + numComputer;
 
 // inserisco il valore della somma come valore che la funzione deve controllare
 
